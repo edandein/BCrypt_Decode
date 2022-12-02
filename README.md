7fff68e0d5c10fb6dbed371c141b05c94df1c2d5
edandein <dcb2525123@163.com>
2022-12-02 16:58:32 +0800

    v1.0

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# 在对应位置修改相应参数即可
+# 在对应位置修改相应参数即可,后续会更新！！！
+
+
+
+Just modify the corresponding parameters at the corresponding position, which will be updated later.
+
--- bcrypter.py
+++ bcrypter.py
@@ -1,9 +1,15 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# author: ed
 import time
 import bcrypt
 from multiprocessing import Pool
 
 
-def decoder(value):
+"""
+核心功能
+"""
+def core(value):
     try:
         hash_value = 'hash'
         check_bc = bcrypt.checkpw(value.encode(), hash_value.encode())
@@ -13,17 +19,22 @@ def decoder(value):
             exit()
         else:
             print('\n[✰] F: ' + str(value))
-        time.sleep(2)
+        time.sleep(2) # 睡眠一段时间，减小cpu压力
     except Exception as err:
         print(err)
 
 
 if __name__ == "__main__":
+    """
+    加载字典
+    """
     path = "./passwd.txt"
     with open(path, "r") as f:
         passwd = [i.strip() for i in f.readlines()]
-
+    """
+    启用线程池
+    """
     pass_pool = Pool(20)
-    pass_pool.map(decoder, passwd)
+    pass_pool.map(core, passwd)
     pass_pool.close()
     pass_pool.join()
